815260218e10277a8419066321d9b0dfa7c0644a
selina <liu.lg@163.com>
2016-06-10 14:30:09 +0800

    初始化sublime workspace 到 github

---
+++ llg/python_demo1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# coding = utf-8
+
+def  change( mylist):
+	"xiugai chuangru canshu liebiao 积极maladflsjflsjfll积极  计划寂静积极家了"
+	mylist.append([1,2,3,4]);
+	print "new mylist  --->",mylist
+	return
+ 
